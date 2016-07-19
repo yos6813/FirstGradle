fff57f4339cb973f83f915fc019d906305be697b
java <java>
2016-07-19 16:28:56 +0900

    10ea Hello and Label change

--- src/main/java/com/hybrid/MainApplication.java
+++ src/main/java/com/hybrid/MainApplication.java
@@ -2,8 +2,10 @@ package com.hybrid;
 
 public class MainApplication{
 	public static void main(String[] args){
-		System.out.println("##############################");
-		System.out.println("Hello First Gradle!");
-		System.out.println("##############################");
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@");
+		for(int i=0; i<10; i++){
+			System.out.println("Hello First Gradle!");
+		}
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@");
 	}
 }
